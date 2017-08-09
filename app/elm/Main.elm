0236029b80e7e894e922c19d44a7876ded199175
Tim DuBois <timothy.dubois@chalmers.se>
2017-08-09 12:28:16 +0200

    Add identicon example

--- app/elm/Main.elm
+++ app/elm/Main.elm
@@ -1,4 +1,73 @@
 module Main exposing (..)
-import Html exposing (text)
+
+import Html exposing (Html, Attribute)
+import Html.Attributes as HA
+import Html.Events as HE
+import Identicon exposing (identicon)
+
+
 main =
-  text "This is Oration."
+    Html.beginnerProgram
+        { model = init
+        , update = update
+        , view = view
+        }
+
+
+type alias Model =
+    String
+
+
+init : Model
+init =
+    "Hello!"
+
+
+type alias Msg =
+    String
+
+
+update : Msg -> Model -> Model
+update text model =
+    text
+
+
+view : Model -> Html Msg
+view model =
+    let
+        field =
+            Html.input
+                [ HA.placeholder "Enter a string..."
+                , HE.onInput identity
+                , inputStyle
+                ]
+                []
+
+        icon =
+            Html.div [ iconStyle ] [ identicon "200px" model ]
+    in
+        Html.div [] [ field, icon ]
+
+
+inputStyle : Attribute Msg
+inputStyle =
+    HA.style
+        [ ( "width", "100%" )
+        , ( "height", "40px" )
+        , ( "padding", "10px 0" )
+        , ( "font-size", "2em" )
+        , ( "text-align", "center" )
+        ]
+
+
+iconStyle : Attribute Msg
+iconStyle =
+    HA.style
+        [ ( "width", "200px" )
+        , ( "height", "200px" )
+        , ( "padding", "50px 0" )
+        , ( "margin", "auto" )
+        , ( "font-size", "2em" )
+        , ( "text-align", "center" )
+        ]
+

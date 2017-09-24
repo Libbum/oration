738a1026ba99292b63af9e63df3db1bfa8b0ec54
Tim DuBois <tim@neophilus.net>
2017-09-24 22:52:57 +0200

    Find closest h1 for blog title, or fallback to document.title

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,20 @@
 import Elm from './main';
-const elmDiv = document.querySelector('#elm-container');
+const elmDiv = document.getElementById('elm-container');
 if (elmDiv) {
     var app = Elm.Main.embed(elmDiv);
-    app.ports.title.send(document.title);
+
+    function closestTitle(el) {
+        var previous;
+        // traverse previous elements to look for a h1
+        while (el) {
+            previous = el.previousElementSibling;
+            if (previous && previous.tagName == "H1") {
+                return previous.innerText;
+            }
+            el = previous;
+        }
+        return document.title; //If not found, use the page title
+    }
+
+    app.ports.title.send(closestTitle(elmDiv));
 }

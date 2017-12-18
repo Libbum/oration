dcf5a82b6827e2de49f557566dbd6ea2114af132
Tim DuBois <tim@neophilus.net>
2017-12-18 12:49:41 +0100

    Indenting was a bit fubar

--- app/elm/Native/Markdown.js
+++ app/elm/Native/Markdown.js
@@ -3,69 +3,64 @@ var _Libbum$oration$Native_Markdown = function() {
 
     // VIRTUAL-DOM WIDGETS
 
-    function toHtml(options, factList, rawMarkdown)
-    {
-	var model = {
-	    options: options,
-	    markdown: rawMarkdown
-	};
-	return _elm_lang$virtual_dom$Native_VirtualDom.custom(factList, model, implementation);
+    function toHtml(options, factList, rawMarkdown) {
+        var model = {
+            options: options,
+            markdown: rawMarkdown
+        };
+        return _elm_lang$virtual_dom$Native_VirtualDom.custom(factList, model, implementation);
     }
 
 
     // WIDGET IMPLEMENTATION
 
     var implementation = {
-	render: render,
-	diff: diff
+        render: render,
+        diff: diff
     };
 
-    function render(model)
-    {
-	var html = parse(model.markdown, formatOptions(model.options));
-	var div = document.createElement('div');
-	div.innerHTML = html;
-	return div;
+    function render(model) {
+        var html = parse(model.markdown, formatOptions(model.options));
+        var div = document.createElement('div');
+        div.innerHTML = html;
+        return div;
     }
 
-    function diff(a, b)
-    {
-	if (a.model.markdown === b.model.markdown && a.model.options === b.model.options)
-	{
-	    return null;
-	}
+    function diff(a, b) {
+        if (a.model.markdown === b.model.markdown && a.model.options === b.model.options) {
+            return null;
+        }
 
-	return {
-	    applyPatch: applyPatch,
-	    data: parse(b.model.markdown, formatOptions(b.model.options))
-	};
+        return {
+            applyPatch: applyPatch,
+            data: parse(b.model.markdown, formatOptions(b.model.options))
+        };
     }
 
-    function applyPatch(domNode, data)
-    {
-	domNode.innerHTML = data;
-	return domNode;
+    function applyPatch(domNode, data) {
+        domNode.innerHTML = data;
+        return domNode;
     }
 
 
     // ACTUAL MARKDOWN PARSER
 
     var parse = function() {
-	// catch the parser object regardless of the outer environment.
-	// (ex. a CommonJS module compatible environment.)
-	// note that this depends on markdown-it's implementation of environment detection.
-	var module = {};
-	var exports = module.exports = {};
+        // catch the parser object regardless of the outer environment.
+        // (ex. a CommonJS module compatible environment.)
+        // note that this depends on markdown-it's implementation of environment detection.
+        var module = {};
+        var exports = module.exports = {};
 
-	// markdown-it 8.4.0 https://github.com//markdown-it/markdown-it @license MIT
-	// markdown-it-katex 2.0.3 https://github.com/waylonflinn/markdown-it-katex/ @license MIT
-	return function (text, options) {
-	    const md = require('markdown-it')(options.preset, options),
-		mk = require('markdown-it-katex');
+        // markdown-it 8.4.0 https://github.com//markdown-it/markdown-it @license MIT
+        // markdown-it-katex 2.0.3 https://github.com/waylonflinn/markdown-it-katex/ @license MIT
+        return function(text, options) {
+            const md = require('markdown-it')(options.preset, options),
+                mk = require('markdown-it-katex');
 
-	    md.use(mk);
-	    return md.render(text);
-	};
+            md.use(mk);
+            return md.render(text);
+        };
     }();
 
 
@@ -73,62 +68,55 @@ var _Libbum$oration$Native_Markdown = function() {
 
     function formatOptions(options) {
 
-	function toHighlight(code, lang) {
+        function toHighlight(code, lang) {
+            try {
+                if (!lang && options.defaultHighlighting.ctor && options.defaultHighlighting.ctor === 'Just') {
+                    lang = options.defaultHighlighting._0;
+                }
 
-	    try {
-		if (!lang && options.defaultHighlighting.ctor && options.defaultHighlighting.ctor === 'Just') {
-		    lang = options.defaultHighlighting._0;
-		}
+                if (typeof Prism !== 'undefined' && lang && Prism.languages[lang]) {
+                    return Prism.highlight(code, Prism.languages[lang]);
+                }
 
-		if (typeof Prism !== 'undefined' && lang && Prism.languages[lang]) {
-		    return Prism.highlight(code, Prism.languages[lang]);
-		}
-		
-		return code;
-	    }
-	    catch(e) {
-		return code;
-	    }
-	}
+                return code;
+            } catch (e) {
+                return code;
+            }
+        }
 
-	options.highlight = toHighlight;
+        options.highlight = toHighlight;
 
-	// assign all 'Just' values and delete all 'Nothing' values
-	for (var key in options) {
-	    var val = options[key];
-	    if (!val) {
-		continue;
-	    }
-	    if (val.ctor === 'Just') {
-		options[key] = val._0;
-	    } else if (val.ctor === 'Nothing') {
-		delete options[key];
-	    }
-	}
+        // assign all 'Just' values and delete all 'Nothing' values
+        for (var key in options) {
+            var val = options[key];
+            if (!val) {
+                continue;
+            }
+            if (val.ctor === 'Just') {
+                options[key] = val._0;
+            } else if (val.ctor === 'Nothing') {
+                delete options[key];
+            }
+        }
 
-	if (options.githubFlavored) {
-	    options.preset = 'default';
-	} else {
-	    options.preset = 'commonmark';
-	}
+        if (options.githubFlavored) {
+            options.preset = 'default';
+        } else {
+            options.preset = 'commonmark';
+        }
 
-	if (options.quotes) {
-	    options.quotes =
-		[ options.quotes.doubleLeft
-		    , options.quotes.doubleRight
-		    , options.quotes.singleLeft
-		    , options.quotes.singleRight
-		];
-	}
+        if (options.quotes) {
+            options.quotes = [options.quotes.doubleLeft, options.quotes.doubleRight, options.quotes.singleLeft, options.quotes.singleRight];
+        }
 
-	return options;
+        return options;
     }
 
 
     // EXPORTS
 
     return {
-	toHtml: F3(toHtml)
+        toHtml: F3(toHtml)
     };
 
 }();
